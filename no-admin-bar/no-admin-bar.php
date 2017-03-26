a0df4d2f3b7eb33a58934654fef45ed42c0be836
zliu <zhanliangliu@qq.com>
2017-03-26 15:10:54 +0800

    no admin bar plugin v1.0

---
+++ no-admin-bar/no-admin-bar.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: Disable Annoying Admin Bar
+ * Plugin URI: http://wordpress.zliu.org/disable-admin-bar.html
+ * Description: Remove the annoying admin bar of login users.
+ * Version: 1.0
+ * Author: Zhanliang Liu
+ * Author URI: http://zliu.org
+*/
+show_admin_bar(false);
